a7abfd90843a95344c0e651170b21aa8d6471013
AbdoulAlim idlbi <abdoulalim.idlbi@gmail.com>
2025-06-23 21:57:39 +0200

    added LLAMA3

--- src/bot.js
+++ src/bot.js
@@ -2,6 +2,16 @@ require("dotenv").config();
 const { token } = process.env;
 const Discord = require("discord.js");
 const fs = require("fs");
+const { Ollama } = require("@langchain/ollama");
+const { ChatPromptTemplate } = require("@langchain/core/prompts");
+const { LLMChain } = require("langchain/chains");
+
+const ollama = new Ollama({
+  model: "llama3",
+  requestOptions: {
+    timeout: 120000, // 60 seconds,
+  },
+});
 
 const client = new Discord.Client({
   intents: [
@@ -33,12 +43,74 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("messageCreate", (message) => {
-  switch (message) {
-    case "test":
-      message.channel.send("Working ;) <@" + message.author.id + ">");
-      break;
+client.on("messageCreate", async (msg) => {
+  if (msg.author.bot) return;
+
+  if (msg.mentions.has(client.user)) {
+    let channelmessages = "";
+    const fetchedMessages = await msg.channel.messages.fetch({ limit: 5 });
+    fetchedMessages.forEach((message) => {
+      channelmessages +=
+        message.author.username + ": " + message.content + "\n";
+    });
+
+    await msg.channel.sendTyping();
+    // create prompt then use ollama to get a reply
+    const prompt = ChatPromptTemplate.fromTemplate(
+      `<|begin_of_text|>
+      <|system|>
+     Your a discord AI bot
+      <|user|>
+      {lastmessages}
+      Here is the message you need to reply to: {message}
+      <|assistant|`
+    );
+    console.log("Last 5 messages : " + channelmessages);
+    const chain = new LLMChain({
+      llm: ollama,
+      prompt: prompt,
+    });
+    chain
+      .invoke({ message: msg.content, lastmessages: channelmessages })
+      .then((res) => {
+        console.log(res);
+        msg.channel.send(res.text);
+      });
   }
 });
 
-//Counter bot attack algorithm
+client.on("messageCreate", async (msg) => {
+  if (msg.author.bot || msg.mentions.has(client.user)) return;
+
+  // create prompt then use ollama to get a reply
+  const prompt = ChatPromptTemplate.fromTemplate(
+    `<|begin_of_text|>
+      <|system|>
+      Your a discord AI bot
+      <|user|>
+      {lastmessages}
+      Here is the message you need to reply to: {message}
+      <|assistant|`
+  );
+  const chain = new LLMChain({
+    llm: ollama,
+    prompt: prompt,
+  });
+  // Get last 10 messages in the channel
+  let channelmessages = "";
+  const fetchedMessages = await msg.channel.messages.fetch({ limit: 5 });
+  fetchedMessages.forEach((message) => {
+    channelmessages += message.author.username + ": " + message.content + "\n";
+  });
+  await msg.channel.sendTyping();
+  console.log("Last 5 messages : " + channelmessages);
+  console.log("replying...");
+  chain
+    .invoke({ message: msg.content, lastmessages: channelmessages })
+    .then((res) => {
+      console.log(res);
+      msg.channel.send(res.text);
+    });
+});
+
+// Randomly send a message after a certain time
